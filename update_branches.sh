4a0f6e96bc4f91a03dde73b2c996404c3619ca82
Kyle Bocinsky <bocinsky@gmail.com>
2022-02-24 11:20:21 -0700

    update updater

--- update_branches.sh
+++ update_branches.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-for BRANCH in `ls .git/refs/heads`; do git checkout $BRANCH; git merge origin/main -m 'Merging updates from main program.'; done 
-for BRANCH in `ls .git/refs/heads`; do git push origin $BRANCH; done
+for BRANCH in `ls .git/refs/heads`; do git switch $BRANCH; git pull; git merge origin/main -m 'Merging updates from main program.'; git push; git switch main; done
